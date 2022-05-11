4b5b89530af08e74a092d4f71311c881c0031a83
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-05-11 17:26:15 -0400

    added event.preventDefault();

--- src/app.js
+++ src/app.js
@@ -141,7 +141,7 @@ function displayFahrenheitTemperature(event) {
 }
 
 function displayCelsiusTemperature(event) {
-  
+  event.preventDefault();
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
